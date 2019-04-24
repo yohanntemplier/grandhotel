c3edccf697ca3532f69732d4374fd6ba933aeb62
Andre Dupin <dd.dupin@gmail.com>
2019-04-24 23:51:17 +0200

    suite au changement de structure des tables room caracteristic et photo, refonte totale du room manager pour la partie chambres, idem pour le room controller, et changements mineurs dans l'index pour boucler sur les bons tableaux

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -8,27 +8,24 @@ use App\Model\RoomManager;
 class RoomController extends AbstractController
 {
     /**
-     * returns a triple array with the rooms, the photos, and the caracteristics.
-     * destroys some data that we don't need in the array, to make easier the for in the viewpage
      * @return string
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
      */
     public function index()
     {
+        $photosPerRoom = [];
+        $caracteristicsPerRoom = [];
 
         $roomManager = new RoomManager();
-        $rooms = $roomManager->selectAllTheTables('room_id', 'id', 'room_id');
-
-        $photos = $roomManager->selectAllThePicturesWithoutIds();
-        $caracteristics = $roomManager->selectAllTheCaracteristicsWithoutIds();
-
-        for ($i = 0; $i < count($caracteristics); $i++) {
-            unset($caracteristics[$i]['id']);
-            unset($caracteristics[$i]['room_id']);
+        $rooms = $roomManager->selectTableRoom();
+        foreach ($rooms as $key => $room) {
+            $caracteristicsPerRoom[] = $roomManager->selectCaracteristics($room['name']);
+            $photosPerRoom[] = $roomManager->selectPhotos($room['name']);
         }
-        return $this->twig->render('Rooms/index.html.twig', ['rooms' => $rooms, 'photos' => $photos,
-            'caracteristics' => $caracteristics]);
+
+        return $this->twig->render('Rooms/index.html.twig', ['rooms' => $rooms, 'photos' => $photosPerRoom,
+            'caracteristics' => $caracteristicsPerRoom]);
     }
 }
--- src/Model/RoomManager.php
+++ src/Model/RoomManager.php
@@ -5,7 +5,6 @@ namespace App\Model;
 
 class RoomManager extends AbstractManager
 {
-
     /**
      * Gives the table name
      */
@@ -13,60 +12,48 @@ class RoomManager extends AbstractManager
 
 
     /**
-     * @var string
-     */
-    protected $className;
-    /**
-     * @var string
-     */
-    protected $tableToJoin;
-    /**
-     * @var string
-     */
-    protected $secondTableToJoin;
-
-    /**
      * RoomManager constructor
      */
     public function __construct()
     {
         parent::__construct(self::TABLE);
-        $this->tableToJoin = 'room_photo';
-        $this->secondTableToJoin = 'room_caracteristic';
+    }
+
+
+    /**
+     * Selects the data from the table room
+     * @return array
+     */
+    public function selectTableRoom(): array
+    {
+        return $this->pdo->query('SELECT name, description, price FROM room;')->fetchAll();
     }
 
     /**
-     * get the table rooms joined with the tables photos and caracteristic
-     * @param string $foreignKey
-     * @param string $primaryKey
-     * @param string $secondForeignKey
+     * Selects the caracteristics data for one room
+     * @param string $roomName
      * @return array
      */
-    public function selectAllTheTables(string $foreignKey, string $primaryKey, string $secondForeignKey): array
+    public function selectCaracteristics(string $roomName):array
     {
-        return $this->pdo->query('SELECT * FROM ' . $this->table . ' JOIN ' . $this->tableToJoin . ' ON ' .
-            $this->tableToJoin . '.' . $foreignKey . '=' . $this->table . '.' . $primaryKey . ' JOIN ' .
-            $this->secondTableToJoin . ' ON ' .
-            $this->secondTableToJoin . '.' . $secondForeignKey . '=' . $this->table . '.' . $primaryKey)->fetchAll();
+        return $this->pdo->query(
+            "SELECT  `room_caracteristic`.`caracteristic_name` FROM `room`
+    JOIN `room_caracteristic` ON `room_caracteristic`.`room_id` = `room.id`
+    JOIN `caracteristic` ON `room_caracteristic`.`caracteristic_id` = `caracteristic`.`id` WHERE
+    `room`.`name` = '$roomName';"
+        )->fetchAll();
     }
 
     /**
-     * retourne table photos without ids
+     * Selects the photos for one room
+     * @param string $roomName
      * @return array
      */
-    public function selectAllThePicturesWithoutIds(): array
+    public function selectPhotos(string $roomName):array
     {
-
-        return $this->pdo->query('SELECT photo1, photo2, photo3, photo4 FROM ' . $this->tableToJoin)->fetchAll();
-    }
-
-    /**
-     * get the table caracteristics without ids
-     * @return array
-     */
-    public function selectAllTheCaracteristicsWithoutIds(): array
-    {
-
-        return $this->pdo->query('SELECT * FROM ' . $this->secondTableToJoin)->fetchAll();
+        return $this->pdo->query(
+            "SELECT `room_photo`.`photo_name` FROM `room_photo` JOIN `room` ON
+    `room_photo`.`room_id` = `room`.`id` WHERE `room`.`name` ='$roomName';"
+        )->fetchAll();
     }
 }
