5a2d66935955af05ebfc00787a11f0648ed11417
yohanntemplier <templieryohann@gmail.com>
2019-04-25 10:10:44 +0200

    ajout de la rubrique WE

---
+++ src/Controller/WeekEndController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+class WeekEndController extends AbstractController
+{
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('WeekEnd/index.html.twig');
+    }
+}
