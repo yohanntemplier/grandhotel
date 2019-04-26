7f94f7c912620ad0ea1634d0ffb2df70722e9026
Andre Dupin <dd.dupin@gmail.com>
2019-04-26 09:49:51 +0200

    ReviewManager OK

---
+++ src/Model/ReviewManager.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/03/18
+ * Time: 18:20
+ * PHP version 7
+ */
+
+namespace App\Model;
+
+/**
+ *
+ */
+class ReviewManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'review';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+
+    public function selectAllOnLine()
+    {
+        return $this->pdo->query('SELECT name, comment, grade, date FROM review WHERE online =1;')->fetchAll();
+    }
+}
