09e1336d1d01d8b2f6ffb6cfdb501b0ad7d615b2
Andre Dupin <dd.dupin@gmail.com>
2019-05-01 08:46:29 +0200

    ajout des infos de connection au mail

--- src/Services/SendEmail.php
+++ src/Services/SendEmail.php
@@ -18,33 +18,23 @@ class SendEmail
     public function sendEmail(string $expeditorName, string $expeditorEmail, string $submitedMessage): void
     {
 
-        // Create the SMTP Transport
-        $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'));
-        //the gmail password is unique for my computer, and only for swift mailer. It can't be used somewhere else,
-        //and you can't connect with it on gmail.
-        $transport->setUsername('legrandhotelorleans@gmail.com');
-        $transport->setPassword('fznzeojlxcsnzvou');
-        // Create the Mailer using the created Transport
+        $transport = (new Swift_SmtpTransport(APP_MAIL_HOST, APP_MAIL_PORT, APP_MAIL_ENCRYPTION));
+        $transport->setUsername(APP_MAIL_USER);
+        $transport->setPassword(APP_MAIL_PWD);
+
         $mailer = new Swift_Mailer($transport);
-        // Create a message
         $message = new Swift_Message();
-
-        // Set a "subject"
         $message->setSubject('Message de ' . $expeditorName);
+        $message->setFrom([APP_MAIL_USER => $expeditorName]);
+        $message->addTo(APP_MAIL_USER, 'Vous');
 
-        // Set the "From address"
-        $message->setFrom(['legrandhotelorleans@gmail.com' => $expeditorName]);
 
-        // Set the "To address" [Use setTo method for multiple recipients, argument should be array]
-        $message->addTo('legrandhotelorleans@gmail.com', 'Vous');
-
-        // Set the plain-text "Body"
         $message->setBody($submitedMessage . '
         
         
         Ce message vous a été envoyé par ' . $expeditorName . '. vous pouvez lui répondre sur '
             . $expeditorEmail . ' .');
-        // Send the message
+
         $mailer->send($message);
     }
 }
